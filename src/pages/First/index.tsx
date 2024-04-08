3ea91586fc22e0ea7be44e79d4e9a0e837ba2ea9
Meiyi <ant19950601@gmail.com>
2024-04-08 20:50:08 +1000

    :memo: Update first page

--- src/pages/First/index.tsx
+++ src/pages/First/index.tsx
@@ -1,7 +1,18 @@
+import Arrow from '../../components/Arrow'
+import RoleList from '../../constants/List'
+
 const First = () => (
-  <div>
-    first
-  </div>
+  <main id="First">
+    {RoleList.map((item) => (
+      <div className="role-content" key={item.name} style={{ backgroundColor: item.color }}>
+        <h3 className="role-content-name">{item.name}</h3>
+        <h4 className="role-content-role">{item.role}</h4>
+        <img src={`/images/${item.name}.png`} alt={item.name} />
+        <Arrow />
+      </div>
+    ))}
+
+  </main>
 )
 
 export default First
