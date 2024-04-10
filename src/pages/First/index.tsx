ae01c1b039472dff23acef90a5ae9adbc653db0e
Meiyi <ant19950601@gmail.com>
2024-04-10 22:02:46 +1000

    :memo: Add mouse enter and leave style

--- src/pages/First/index.tsx
+++ src/pages/First/index.tsx
@@ -1,18 +1,41 @@
+import { useRef, useState } from 'react'
+
 import Arrow from '../../components/Arrow'
 import RoleList from '../../constants/List'
 
-const First = () => (
-  <main id="First">
-    {RoleList.map((item) => (
-      <div className="role-content" key={item.name} style={{ backgroundColor: item.color }}>
-        <h3 className="role-content-name">{item.name}</h3>
-        <h4 className="role-content-role">{item.role}</h4>
-        <img src={`/images/${item.name}.png`} alt={item.name} />
-        <Arrow />
-      </div>
-    ))}
+const First = () => {
+  const roleItemRef = useRef<(HTMLDivElement | null)[]>([])
+  const [showRole, setShowRole] = useState<string | null>(null)
 
-  </main>
-)
+  const handleMouseEnter = (name: string) => {
+    setShowRole(name)
+  }
+
+  const handleMouseLeave = () => {
+    setShowRole(null)
+  }
+  return (
+    <main id="First">
+      {RoleList.map((item) => (
+        <div
+          className="role-content"
+          key={item.name}
+          style={{ backgroundColor: showRole === item.name ? item.color : '#000' }}
+          ref={(el) => { roleItemRef.current = [...roleItemRef.current, el] }}
+          onMouseEnter={() => handleMouseEnter(item.name)}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div className="role-content-title">
+            <h3 className="role-content-name">{item.name}</h3>
+            <h4 className="role-content-role">{item.role}</h4>
+          </div>
+          <img src={`/images/${item.name}.png`} alt={item.name} />
+          <Arrow />
+        </div>
+      ))}
+
+    </main>
+  )
+}
 
 export default First
