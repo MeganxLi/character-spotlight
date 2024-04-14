e752927be35ead9d0f9f2b7e0a17a8fe0eaaa950
Meiyi <ant19950601@gmail.com>
2024-04-14 22:30:39 +1000

    :lipstick: Scss change Styled-Component css

---
+++ src/styled/pages/First.ts
@@ -0,0 +1,77 @@
+import styled from 'styled-components'
+
+import { rwd } from '../util/Mixins'
+
+export const FirstPage = styled.main`
+ height: 100vh;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  transition: 2s;
+
+  ${rwd('tablet')} {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  ${rwd('mobile')} {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`
+
+export const RoleContent = styled.div<RoleContentType>`
+  background: ${(props) => props.backgroundColor};
+  color: #fff;
+  margin: 2px;
+  padding: 2.5rem 1rem;
+  transition: 0.5s;
+  display: grid;
+  align-items: center;
+
+  img {
+    width: 100%;
+    object-fit: cover;
+    transform: translateY(20px);
+    transition: transform 0.3s ease;
+    opacity: 0;
+  }
+
+  &:hover {
+    #ArrowIcon {
+      transform: rotate(-60deg);
+    }
+
+    img {
+      transform: translateY(0);
+      opacity: 1;
+    }
+
+    color: #000;
+  }
+`
+
+export const RoleContentTitle = styled.div``
+
+export const RoleContentName = styled.h3`
+  font-size: 3.5rem;
+  font-weight: bold;
+  text-transform: Uppercase;
+  margin-bottom: 0.2rem;
+`
+
+export const RoleContentRole = styled.h4`
+  font-size: 2.5rem;
+  margin-bottom: 0.5rem;
+`
+
+const IconsSize = '2.5rem'
+export const ArrowIcon = styled.div`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  width: ${IconsSize};
+  height: ${IconsSize};
+  border: 2px solid #fff;
+  border-radius: 50px;
+  transition: 0.3s;
+  margin-top: 2rem;
+`
