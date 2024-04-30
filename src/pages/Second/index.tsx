d8c9dd8856abd827d7254c4f76f1d8d2f51c40d8
Meiyi <ant19950601@gmail.com>
2024-05-01 00:33:09 +1000

    :memo: Update Second page file

--- src/pages/Second/index.tsx
+++ src/pages/Second/index.tsx
@@ -1,7 +1,47 @@
-const Second = () => (
-  <div>
-    first
-  </div>
-)
+import { useEffect, useState } from 'react'
+
+import Switch from '../../components/Switch'
+import { PUBLIC_URL } from '../../constants/EnumType'
+import RoleList from '../../constants/List'
+import { SecondPage, SecondRoleContent } from '../../styled/pages/Second'
+
+const Second = () => {
+  const [clickRole, setClickRole] = useState<number | null>(null)
+
+  const clickItem = (itemKey: number) => (clickRole !== itemKey && setClickRole(itemKey))
+
+  useEffect(() => {
+    console.log('clickRole--', clickRole)
+  }, [clickRole])
+
+  return (
+    <SecondPage>
+      {RoleList.map((item, key) => (
+        <SecondRoleContent
+          key={item.name}
+          onClick={() => clickItem(key)}
+          $backgroundcolor={item.color}
+          $img={`/${PUBLIC_URL}/images/${item.name}.png`}
+        >
+          {clickRole === key && (
+            <Switch
+              next={false}
+              current={clickRole || 0}
+              onChange={(calcCurrent) => setClickRole(calcCurrent)}
+            />
+          )}
+          {/* <img src={`/${PUBLIC_URL}/images/${item.name}.png`} alt={item.name} /> */}
+          {clickRole === key && (
+            <Switch
+              next
+              current={clickRole || 0}
+              onChange={(calcCurrent) => setClickRole(calcCurrent)}
+            />
+          )}
+        </SecondRoleContent>
+      ))}
+    </SecondPage>
+  )
+}
 
 export default Second
