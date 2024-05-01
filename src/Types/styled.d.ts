92ca35e0f221da5c00053bdeb851bde1f206cf39
Meiyi <ant19950601@gmail.com>
2024-05-02 02:04:10 +1000

    :label: Update styled typescript

--- src/Types/styled.d.ts
+++ src/Types/styled.d.ts
@@ -1,10 +1,15 @@
 /* -- First --*/
 type RoleContentType = {
   $backgroundcolor: string
-  $img?: string
 }
 
 /* -- Second --*/
 type SwitchType = {
   $nextDirection: boolean
+}
+
+type SecondRoleContentType = RoleContentType & {
+  $img: string
+  $clickRole: number | null
+  $key: number
 }
