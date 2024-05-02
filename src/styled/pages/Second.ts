4b4333815ac5a88032d7d663b327879c58a78d2a
Meiyi <ant19950601@gmail.com>
2024-05-03 02:27:10 +1000

    :lipstick: Update Second grid transition

--- src/styled/pages/Second.ts
+++ src/styled/pages/Second.ts
@@ -17,11 +17,40 @@ export const SwitchStyled = styled(CircleStyled) <SwitchType>`
 `
 
 // Page
-export const SecondPage = styled.main`
+const gridWidth = '0.2fr'
+const showGridTemplate = (current: number | null) => {
+  // The "repeat" setting cannot be configured for animations; it needs to be set individually.
+  // let templateStyled = ''
+  // if (current === null) {
+  //   templateStyled = '1fr 1fr 1fr 1fr'
+  // } else {
+  //   templateStyled = '1fr'
+  //   for (let i = 1; i < RoleList.length; i++) {
+  //     templateStyled += ' 150px'
+  //   }
+  //   // templateStyled += ` ${'1fr '.repeat(RoleList.length - 1)}`
+  // }
+
+  // return templateStyled
+
+  switch (current) {
+    case 0:
+      return '1fr 0fr 0fr 0fr'
+    case 1:
+      return `${gridWidth} 1fr 0fr 0fr`
+    case 2:
+      return `${gridWidth} ${gridWidth} 1fr 0fr`
+    case 3:
+      return `${gridWidth} ${gridWidth} ${gridWidth} 1fr`
+    default:
+      return '1fr 1fr 1fr 1fr'
+  }
+}
+export const SecondPage = styled.main<SecondPageType>`
   background-color:${colors.black};
   height:100vh;
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: ${({ $current }) => showGridTemplate($current)};
   transition: 2s;
 `
 
@@ -31,7 +60,7 @@ export const SecondRoleContent = styled.div<SecondRoleContentType>`
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center 5rem;
-  display: ${({ $clickRole, $key }) => ($clickRole !== null && $clickRole <= $key - 1 ? 'none' : 'block')};
+  /* display: ${({ $clickRole, $key }) => ($clickRole !== null && $clickRole <= $key - 1 ? 'none' : 'block')}; */
   img {
     width: 100%;
     object-fit: cover;
