398d43e8c2d17a2d7dd1d443c8f4c5a2c515a76b
Meiyi <ant19950601@gmail.com>
2024-05-04 18:21:32 +1000

    :arrow_up: Add a styled function for future additions.

--- src/styled/pages/Second.ts
+++ src/styled/pages/Second.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 
+import RoleList from '../../constants/List'
 import CircleStyled from '../components/Circle'
 import colors from '../util/Variables'
 
@@ -18,40 +19,35 @@ export const SwitchStyled = styled(CircleStyled) <SwitchType>`
 
 // Page
 const gridWidth = '0.2fr'
-const showGridTemplate = (current: number | null) => {
+const generateGridTemplate = (current: number | null) => {
   // The "repeat" setting cannot be configured for animations; it needs to be set individually.
-  // let templateStyled = ''
-  // if (current === null) {
-  //   templateStyled = '1fr 1fr 1fr 1fr'
-  // } else {
-  //   templateStyled = '1fr'
-  //   for (let i = 1; i < RoleList.length; i++) {
-  //     templateStyled += ' 150px'
-  //   }
-  //   // templateStyled += ` ${'1fr '.repeat(RoleList.length - 1)}`
-  // }
+  const templateValues = []
 
-  // return templateStyled
-
-  switch (current) {
-    case 0:
-      return '1fr 0fr 0fr 0fr'
-    case 1:
-      return `${gridWidth} 1fr 0fr 0fr`
-    case 2:
-      return `${gridWidth} ${gridWidth} 1fr 0fr`
-    case 3:
-      return `${gridWidth} ${gridWidth} ${gridWidth} 1fr`
-    default:
-      return '1fr 1fr 1fr 1fr'
+  for (let i = 0; i < RoleList.length; i++) {
+    switch (true) {
+      case current === null:
+        templateValues.push('1fr')
+        break
+      case i === current:
+        templateValues.push('1fr')
+        break
+      case i < current!:
+        templateValues.push(gridWidth)
+        break
+      default:
+        templateValues.push('0fr')
+        break
+    }
   }
+
+  return templateValues.join(' ')
 }
 export const SecondPage = styled.main<SecondPageType>`
   background-color:${colors.black};
   height:100vh;
   display: grid;
-  grid-template-columns: ${({ $current }) => showGridTemplate($current)};
-  transition: 2s;
+  grid-template-columns: ${({ $current }) => generateGridTemplate($current)};
+  transition: 0.8s;
 `
 
 export const SecondRoleContent = styled.div<SecondRoleContentType>`
@@ -60,7 +56,6 @@ export const SecondRoleContent = styled.div<SecondRoleContentType>`
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center 5rem;
-  /* display: ${({ $clickRole, $key }) => ($clickRole !== null && $clickRole <= $key - 1 ? 'none' : 'block')}; */
   img {
     width: 100%;
     object-fit: cover;
